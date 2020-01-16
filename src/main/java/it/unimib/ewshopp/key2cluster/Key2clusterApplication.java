699ae78cad8e7291928d1a6fd0323057553c4fa1
miciav <michele.ciavotta@gmail.com>
2020-01-16 09:31:48 +0100

    first commit

---
+++ src/main/java/it/unimib/ewshopp/key2cluster/Key2clusterApplication.java
@@ -0,0 +1,13 @@
+package it.unimib.ewshopp.key2cluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Key2clusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Key2clusterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unimib/ewshopp/key2cluster/Key2clusterApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimib.ewshopp.key2cluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Key2clusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
