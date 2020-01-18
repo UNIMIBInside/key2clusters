4a30ae7f6a5b727f317c8cf5a3b39fbfa5ac704b
miciav <michele.ciavotta@gmail.com>
2020-01-18 16:53:23 +0100

    Sample file added

--- src/test/java/it/disco/unimib/ewshopp/key2cluster/Key2ClusterApplicationTestsIT.java
+++ src/test/java/it/disco/unimib/ewshopp/key2cluster/Key2ClusterApplicationTestsIT.java
@@ -3,11 +3,13 @@ package it.disco.unimib.ewshopp.key2cluster;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import it.disco.unimib.ewshopp.key2cluster.components.IDataManager;
 import lombok.extern.java.Log;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -27,6 +29,11 @@ import static io.restassured.RestAssured.given;
         "spring.redis.port=6379", "keyword.cluster.recreate-data-structure=false"})
 @Log
 public class Key2ClusterApplicationTestsIT {
+
+    @Autowired
+    private IDataManager dataManager;
+
+
     @LocalServerPort
     private int port;
     private String baseURI;
@@ -43,14 +50,13 @@ public class Key2ClusterApplicationTestsIT {
     public static DockerComposeContainer compose =
             new DockerComposeContainer(
                     new File("src/test/docker/docker-compose.yml"))
-                    .withExposedService("redis_1", 6379,  Wait.forListeningPort());
-
-
+                    .withExposedService("redis_1", 6379,  Wait.defaultWaitStrategy());
 
 
 
     @Test
     public void getCategoriesPerKeyword() {
+        System.out.println("number of keywords: " +dataManager.countKeywords());
         Response response = given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
